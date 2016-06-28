b1bf06c44cc5ef05deee58f1e8b2567403ab91b4
Ed Giles <ed@admedo.com>
2016-06-28 18:16:09 +0100

    updated for error:

--- src/js/editable-element/controller.js
+++ src/js/editable-element/controller.js
@@ -357,7 +357,7 @@ angular.module('xeditable').factory('editableController',
       // bind blur for when really losing focus
       // on single inputs
       self.inputEl.bind('blur', function(e) {
-          $event.stopPropagation();
+          e.stopPropagation();
           if(!self.single) {
             return;
           }
